d7bfeaedcde75919b995933a3c2a5c001b9938b8
xuzhewei <xzhewei@gmail.com>
2019-03-11 16:54:13 +0800

    filter ignore object generate roadline

--- script/generate_road_line_annotation.py
+++ script/generate_road_line_annotation.py
@@ -8,7 +8,12 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-json_file = '../output/json/scut_train_10x.json'
+# json_file = '../output/json/caltech_train_10x.json'
+# json_file = '../output/json/caltech_test_1x.json'
+# json_file = '../output/json/scut_train_10x.json'
+# json_file = '../output/json/scut_test_1x.json'
+# json_file = '../output/json/kaist_train_all_10x.json'
+json_file = '../output/json/kaist_test_all_1x.json'
 
 scut_train_json = pdt.load_json(json_file)
 
@@ -16,8 +21,9 @@ scut_train = COCO(json_file)
 
 for index, img in enumerate(scut_train_json['images']):
     anns = scut_train.imgToAnns[img['id']]
-    if len(anns):
-        y_centers = [ann['bbox'][1]+ann['bbox'][3]/2 for ann in anns]
+    anns_f = [ann for ann in anns if not ann['iscrowd']]
+    if len(anns_f):
+        y_centers = [ann['bbox'][1]+ann['bbox'][3]/2 for ann in anns_f]
     else:
         y_centers = -1
     # simple average center
@@ -34,12 +40,12 @@ for img in scut_train_json['images']:
 logging.debug("Done.")
 
 output_name,ext = os.path.splitext(json_file)
-output_name = output_name + '_roadline' + ext
+output_name = output_name + '_roadline_pure' + ext
 logging.debug("Save json to {}.".format(output_name))
 pdt.save_json(scut_train_json,output_name)
 
 output_name,ext = os.path.splitext(json_file)
-output_name = output_name + '_roadline_only' + ext
+output_name = output_name + '_roadline_only_pure' + ext
 logging.debug("Save roadline json to {}.".format(output_name))
 pdt.save_json(y_centers,output_name)
 

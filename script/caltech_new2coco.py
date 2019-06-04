00530b711f58931d529db88bfddb86190cc62bcc
xuzhewei <xzhewei@gmail.com>
2019-06-04 14:28:33 +0800

    fix error

--- script/caltech_new2coco.py
+++ script/caltech_new2coco.py
@@ -8,12 +8,12 @@ import pydatatool as pdt
 
 pth = "/home/all/datasets/caltech/extract/train03_new"
 annotations_train_10x, image_ids_train, annId_str, objId_str = pdt.caltech.txts2cocos(pth,0,0,{})
-pdt.scut.save_coco(annotations_train_10x,image_ids_train,'../output/json/caltech_train_10x_new.json')
+pdt.caltech.save_coco(annotations_train_10x,image_ids_train,'../output/json/caltech_train_10x_new.json')
 
 
 pth = "/home/all/datasets/caltech/extract/test_1x_new"
 annotations_test_1x, image_ids_test, annId_str, objId_str = pdt.caltech.txts2cocos(pth,annId_str,objId_str,{})
-pdt.scut.save_coco(annotations_test_1x,image_ids_test,'../output/json/caltech_test_1x_new.json')
+pdt.caltech.save_coco(annotations_test_1x,image_ids_test,'../output/json/caltech_test_1x_new.json')
 
 
 igandic = [a['ignore']==a['iscrowd'] for a in annotations_test_1x]
